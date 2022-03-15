5fc6c7ea6ddf5c2726ae3119a46a532533b86b21
Remelie Bongcawil <remeliebongcawil20@gmail.com>
2022-03-15 10:55:35 +0800

    git commit welcome.php -m "changed the name to my fullname"

--- welcome.php
+++ welcome.php
@@ -3,7 +3,9 @@
 <title>Welcome!</title>
 </head>
         <body>
-            <?php echo "Hello, I'm Remelie Bongcawil. I'm new to git. How about you?"; ?>
+                <?php echo "Hello, I'm Remelie Bongcawil. I'm new to git. How about you?"; ?>
+
+                
             <a href="index.php">home</a>
         </body>
 </html>
